26832fb766fc4334b88cd1d9c6237f769f716ec2
Daniel Saraiva <saraivadaniel@protonmail.com>
2022-06-02 10:57:45 -0300

    Sensor sonico direita

---
+++ SensorSonicoVirarDireita/SensorSonicoVirarDireita.c
@@ -0,0 +1,19 @@
+#define _TASK_1_ 1
+#include "KYLib.h"
+#include "stm32f10x.h"
+#include "taskMange.h"
+void task1(void){
+  while (1) {
+    if((Ultrasound(1))<(50)) {
+      SetMotor(1,0);
+      SetMotor(2,0);
+      SetWaitForTime(1);
+      SetMotor(1,50);
+      SetMotor(2,0);
+      SetWaitForTime(1);
+    } else {
+      SetMotor(1,30);
+      SetMotor(2,30);
+    }
+  }
+}
