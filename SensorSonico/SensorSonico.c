a069b3a1984b78a3265606baea47f4ab310f0447
Daniel Saraiva <saraivadaniel@protonmail.com>
2022-06-02 10:44:21 -0300

    Sensor sonico - melhorias

--- SensorSonico/SensorSonico.c
+++ SensorSonico/SensorSonico.c
@@ -3,11 +3,13 @@
 #include "stm32f10x.h"
 #include "taskMange.h"
 void task1(void){
-  if((Ultrasound(1))<(50)) {
-    SetMotor(1,0);
-    SetMotor(2,0);
-  } else {
-    SetMotor(1,100);
-    SetMotor(2,100);
+  while (1) {
+    if((Ultrasound(1))<(50)) {
+      SetMotor(1,0);
+      SetMotor(2,0);
+    } else {
+      SetMotor(1,100);
+      SetMotor(2,100);
+    }
   }
 }
